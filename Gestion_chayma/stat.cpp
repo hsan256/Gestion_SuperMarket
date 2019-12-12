16ab96800fb52d7da54f604abeaeffdb173acfd8
Shaymaaaaa <47744919+Shaymaaaaa@users.noreply.github.com>
2019-12-11 17:29:10 -0700

    stat_plus

--- Gestion_chayma/stat.cpp
+++ Gestion_chayma/stat.cpp
@@ -7,6 +7,7 @@
 #include <QMessageBox>
 #include <QSqlQuery>
 #include <QtCharts/QPieSlice>
+#include <QtCharts/QPieSeries>
 
 
 QT_CHARTS_USE_NAMESPACE
@@ -36,6 +37,13 @@ QChartView * Stat::Preparechart(){
 
        series->append("service_jour", count1);
        series->append("service_nuit", count2);
+       QPieSlice * slice1=series->slices().at(0);
+       slice1->setExploded();
+       slice1->setPen(QPen(Qt::black,1));
+       slice1->setBrush(Qt::darkRed);
+       slice1->setLabelVisible();
+       QPieSlice * slice2=series->slices().at(1);
+       slice2->setLabelVisible();
        chart = new QChart();
        chart->addSeries(series);
        chart->setTitle("Les Types");
